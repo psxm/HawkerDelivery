8df4cd784eb4c5e1a03b8dc8dd95fb9b96fb525b
psxm <teo.michelle.wt@gmail.com>
2016-04-07 23:12:25 +0800

    ViewByHawkerActivity.java updated

--- app/src/main/java/com/colon3/cz2006/hawkerdelivery/Boundary/ViewByHawkerActivity.java
+++ app/src/main/java/com/colon3/cz2006/hawkerdelivery/Boundary/ViewByHawkerActivity.java
@@ -6,15 +6,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
 
+import com.colon3.cz2006.hawkerdelivery.Adapter.HawkerCentreAdapter;
+import com.colon3.cz2006.hawkerdelivery.Controller.HawkerCentreController;
+import com.colon3.cz2006.hawkerdelivery.Entity.HawkerCentre;
 import com.colon3.cz2006.hawkerdelivery.R;
 
+import java.util.ArrayList;
+
 public class ViewByHawkerActivity extends AppCompatActivity {
+    private ListView list;
+    public ArrayList<HawkerCentre> hcArrayList = new ArrayList<HawkerCentre>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_by_hawker);
+        setTitle("Hawker Centres");
+
+        viewList();
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,4 +39,12 @@ public class ViewByHawkerActivity extends AppCompatActivity {
         });*/
     }
 
+    public void viewList() {
+        list = (ListView) findViewById(R.id.hawker_centre_listview);
+        HawkerCentreController controller = new HawkerCentreController();
+
+        hcArrayList = controller.allHawkerCentres();
+        HawkerCentreAdapter adapter = new HawkerCentreAdapter(hcArrayList, this);
+        list.setAdapter(adapter);
+    }
 }
